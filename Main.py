20ebe195076a5bd56fba64a7c053d5da325dc89d
BNNorman <brian.n.norman@gmail.com>
2018-04-11 14:36:44 +0100

    Added import for Constants.py to pick up ARENA, CONSOLE and MANAGER definitions thus keeping all configuration stuff in one place.

--- Main.py
+++ Main.py
@@ -6,16 +6,21 @@ import time
 t0=time.clock()
 from tkinter import *
 from Forms import ScriptManager
+from Constants import *
 t1=time.clock()
 print("Main imports took %.6fs"%(t1-t0))
 
 
-ARENA=(800,800)
 CONSOLE=(600,150)
 MANAGER=(600,800)
 FPS=50
 
+root=Tk()
 
-sm=ScriptManager.ScriptManager(parent=Tk(),size=MANAGER,arena=ARENA,consoleSize=CONSOLE ,fps=FPS,
+sm=ScriptManager.ScriptManager(parent=root,size=MANAGER,arena=ARENA,consoleSize=CONSOLE ,fps=FPS,
                                robotConfig="robot.json")
 sm.show() # no return till program is stopped
+
+root.destroy()
+
+print("Program closed")
